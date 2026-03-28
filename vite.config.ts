a22852d0bfc7339a670e0a3fa5a7510a165e2752
aks <akstewart@protonmail.com>
2026-03-28 12:52:10 +0900

    new icons

--- vite.config.ts
+++ vite.config.ts
@@ -40,9 +40,20 @@ export default defineConfig(({ command }) => {
 					orientation: "portrait",
 					icons: [
 						{
-							src: "/icon-192.svg",
-							sizes: "any",
-							type: "image/svg+xml",
+							src: "/icon-192.png",
+							sizes: "192x192",
+							type: "image/png",
+						},
+						{
+							src: "/icon-512.png",
+							sizes: "512x512",
+							type: "image/png",
+						},
+						{
+							src: "/icon-512.png",
+							sizes: "512x512",
+							type: "image/png",
+							purpose: "maskable",
 						},
 					],
 				},
